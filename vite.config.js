99f29f62837e3e2e9efe8e146b47eb1e917fe4bd
Alex Pumari <alexpumari0@gmail.com>
2025-03-23 21:01:58 -0300

    [Avoid] Deploy for Git Hub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: 'https://alexrubenpumari.github.io/web-clon-sf-calistenia/react-to-do-list',
 })
